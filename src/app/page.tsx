cb75e8db2c7ed81c6dafe5f56d10aff3506f1659
umershaikh123 <umershaikh217@gmail.com>
2023-10-10 15:12:53 +0500

    fix : frame type error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
           height="315"
           src="https://www.youtube.com/embed/BgCgauWVTs0?si=Dwo48KuAvSThBBdK"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
 
